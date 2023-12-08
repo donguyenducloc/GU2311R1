1f32db7932c140662d94b712371929159e93126e
donguyenducloc <152408480+donguyenducloc@users.noreply.github.com>
2023-12-08 12:50:07 +0700

    Hien thi thoi gian hien tai

---
+++ Baitap2/SystemTime/Program.cs
@@ -0,0 +1,13 @@
+﻿using System;
+namespace SystemTime
+{
+internal class Program
+{
+    private static void Main(string[] args)
+    {
+       DateTime localTime = DateTime.Now;
+       Console.WriteLine("Thoi gian hien tai la: " + localTime);
+    }
+
+}
+}
