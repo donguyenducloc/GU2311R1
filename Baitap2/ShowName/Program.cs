0f50607f7fd301e8c935a056b390988eacbbf68e
donguyenducloc <152408480+donguyenducloc@users.noreply.github.com>
2023-12-09 13:48:40 +0700

    Hien thi loi chao

---
+++ Baitap2/ShowName/Program.cs
@@ -0,0 +1,14 @@
+﻿using System;
+namespace SystemTime
+{
+internal class Program
+{
+    private static void Main(string[] args)
+    {
+       Console.WriteLine("Hay nhap ten cua ban");
+       string myName = Console.ReadLine();
+       Console.WriteLine("Ten toi la:" + myName);
+    }
+
+}
+}
