dd226b7d0a750fcfb4fabfb2c359ff04ebbecefb
donguyenducloc <152408480+donguyenducloc@users.noreply.github.com>
2023-12-09 13:42:42 +0700

    Chuyen doi tien te

---
+++ Baitap2/ShowCurrent/Program.cs
@@ -0,0 +1,18 @@
+﻿using System;
+
+namespace UsingCurrent
+{
+ class Program
+ {
+     static void Main(string[] args)
+    {
+       Console.WriteLine("Nhap ty gia");
+       float usd = float.Parse(Console.WriteLine());
+       Console.WriteLine("Nhap So USD ban co:");
+       float myUsd = float.Parse(Console.WriteLine());
+       Console.WriteLine("Nhap So tien viet ban co sau khi doi la:"+ usd*myUsd);
+       
+    }
+
+ }
+}
