7a1bde4870056930b51210106b621ff28d5d03b7
donguyenducloc <152408480+donguyenducloc@users.noreply.github.com>
2023-12-11 12:26:41 +0700

    bai tap hoan thanh

---
+++ Baitap2/PTBacnhat/Program.cs
@@ -0,0 +1,21 @@
+﻿
+     class Program
+{
+    private static void Main(string[] args)
+    {
+      Console.WriteLine("Nhap so a");
+      Double a = Convert.ToDouble(Console.ReadLine());
+      Console.WriteLine("Nhap so b");
+      Double b = Convert.ToDouble(Console.ReadLine());
+
+      
+      if(a!=0)  {Console.WriteLine("Pt co nghiem" + -b/a);}
+       
+      else 
+      {
+        if(b==0)  Console.WriteLine("Pt vo so nghiem");
+        else
+        {Console.WriteLine("Pt vo nghiem");}
+      }
+    }
+}
