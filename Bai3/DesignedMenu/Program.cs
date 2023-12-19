f8b1670ed78aac9edee29148f86e45572b259bfa
donguyenducloc <152408480+donguyenducloc@users.noreply.github.com>
2023-12-19 19:18:36 +0700

    Thiet ke menu cho ung dung

---
+++ Bai3/DesignedMenu/Program.cs
@@ -0,0 +1,45 @@
+﻿
+internal class Program
+{
+    private static void Main(string[] args)
+    {
+      int Choise= -1;
+      while (Choise!=0)
+      {
+        Console.WriteLine("Nhap mot ky tu tu ban phim");
+        Choise = Convert.ToInt32(Console.ReadLine());
+        switch (Choise)
+        {
+            case 1:
+            {
+                Console.WriteLine("******");
+                Console.WriteLine("*****");
+                Console.WriteLine("****");
+                Console.WriteLine("***");
+                Console.WriteLine("**");
+                Console.WriteLine("*");                
+            }
+            break;
+            case 2:
+            {
+                Console.WriteLine("******");
+                Console.WriteLine("******");
+                Console.WriteLine("******");               
+            }
+            break;
+            case 3:
+            {
+                Console.WriteLine("******");
+                Console.WriteLine("******");
+                Console.WriteLine("******");
+                Console.WriteLine("******");
+            }
+            break;
+            default: Console.WriteLine("Nhap lai");
+            break;
+        }
+        
+      }  
+
+    }
+} 
