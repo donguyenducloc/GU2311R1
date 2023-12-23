f173e73094cd9a645af8ecd1b1c53a72ccbd532c
donguyenducloc <152408480+donguyenducloc@users.noreply.github.com>
2023-12-23 21:34:10 +0700

    tim gia tri trong mang

---
+++ W04/FindName/Program.cs
@@ -0,0 +1,31 @@
+﻿ class Program
+{
+    private static void Main(string[] args)
+    {
+       string[] nameList={"Loc","Tai","Thinh","Linh","Vinh","Xuan","Duong","Quy","Duom"};
+       Console.WriteLine("Nhap ten mot ban");
+        string name = Console.ReadLine();
+        bool isTrue=false;
+        for (int i = 0; i <= nameList.Length-1; i++)       
+        {
+           if(nameList[i].Equals(name))
+           {
+             Console.WriteLine($"Co ten trong mang va vi tri so {i+1}");
+             isTrue=true;
+             break;
+           } 
+
+            }
+            if(!isTrue)
+            {
+                Console.WriteLine("Khong có ten");           
+            }
+            {
+          
+                 
+
+
+          
+        }
+    }
+}
