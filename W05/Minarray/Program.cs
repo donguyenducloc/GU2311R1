d38abdee01e2b67aeb0fe77bce10220a00f446ce
donguyenducloc <152408480+donguyenducloc@users.noreply.github.com>
2024-01-06 08:08:14 +0700

    tim gia trị nho nhat trong mang su dung phuong thuc

---
+++ W05/Minarray/Program.cs
@@ -0,0 +1,25 @@
+﻿
+ class Program
+{
+    private static void Main(string[] args)
+    {
+        int[] array = {1,2,3,4,5,6,7,8,9,20,21};
+        Console.WriteLine("gia tri nho nhat la:");
+        int min = array[0];
+       Console.WriteLine(MinFind(array));
+
+    }
+    static int MinFind(int[]array)
+    {
+        int min = array[0];
+        for (int i = 0; i < array.Length; i++)
+        {
+            if(array[i]<min)
+            {
+                min=array[i];
+            }
+            
+        }
+        return min;
+    }
+}
